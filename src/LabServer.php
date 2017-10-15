c709d6924ce3d670f3b63207ba97b11393bb88a0
Erwin <zzplab@users.noreply.github.com>
2017-10-15 15:58:46 +0200

    Update LabServer.php

--- src/LabServer.php
+++ src/LabServer.php
@@ -16,8 +16,49 @@ use Leafo\ScssPhp\Server;
 
 class LabServer extends Server
 {
+	protected $vCss = false;
+	
 	protected function cacheName($fname)
 	{
-		return strstr($fname, '/', true) . '.css';
+		return strstr($fname, '/', true) . $this->getVersion() . '.min.css';
+	}
+	
+	protected function getPathInfo()
+	{
+		$this->path_info = str_replace(['"',"'"],'',strip_tags(strtolower(ltrim($_SERVER['PATH_INFO'],'/'))));
+	}
+	
+	protected function inputName()
+	{
+		$this->getPathInfo();
+		if(strstr($this->path_info, '/'))
+		{
+			list($name, $version) = explode('/', $this->path_info);
+			$this->vCss = $version;
+		}
+		else
+		{
+			$name = $this->path_info;
+		}
+		if(empty($name))
+		{
+			$name = 'app.scss';
+		}
+		elseif(!strstr($name, '.scss'))
+		{
+			$name = strtolower($name).'.scss';
+		}
+		return $name;
+	}
+
+	protected function getVersion()
+	{
+	  switch ($this->vCss) {
+	    case '':
+	      return '';
+	    break;
+	    default:
+	      return '-'.$this->vCss;
+	  }
 	}
 }
